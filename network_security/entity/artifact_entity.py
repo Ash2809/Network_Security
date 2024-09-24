b16bbab9eb9d085f46694959211784cb060efb0c
Ash2809 <aashutoshkumar2809@gmail.com>
2024-09-24 11:41:15 +0530

    artifact skeleton updated

---
+++ network_security/entity/artifact_entity.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+@dataclass
+class Data_ingestion_artifact():
+    pass
+
+@dataclass
+class Data_transformation_artifact():
+    pass
+
+@dataclass
+class Data_validation_artifact():
+    pass
+
+@dataclass
+class Model_Evaluater_artifact():
+    pass
+
+@dataclass
+class Model_trainer_artifact():
+    pass
+
+@dataclass
+class Classification_metric_artifact():
+    pass
+
+@dataclass 
+class Model_pusher_artifact():
+    pass
---
+++ network_security/entity/config_entity.py
@@ -0,0 +1,9 @@
+from dataclasses import dataclass
+
+@dataclass
+class Data_ingestion_config():
+    def __init__(self):
+        pass
+
+@dataclass
+class 
