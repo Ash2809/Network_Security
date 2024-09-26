b30925e1a5fc0cffba6a4bee523e67f060eadc2b
Ash2809 <aashutoshkumar2809@gmail.com>
2024-09-26 15:34:43 +0530

    data_validation skeleton updated

--- network_security/components/data_validation.py
+++ network_security/components/data_validation.py
@@ -1,3 +1,49 @@
+from network_security.entity.artifact_entity import Data_validation_artifact
+from network_security.entity.config_entity import Data_validation_config
+
+from network_security.exceptions.exception import NetworkSecurityException
+from network_security.logger.logger import logging
+
+import pandas as pd
+import os
+import sys
+
 class DataValidation():
     def __init__():
-        pass
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+    def detect_numerical_columns(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+    
+
+    def is_numerical_column_exist(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    @staticmethod
+    def read_data(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    def detect_dataset_drift(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+        
+    
+    def initiate_data_validation(self):
+        try:
+            pass
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
