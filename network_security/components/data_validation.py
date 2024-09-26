8a7f30bb92783c99c788f8c63cc28eedc1b8466c
Ash2809 <aashutoshkumar2809@gmail.com>
2024-09-26 15:38:50 +0530

    data_validation skeleton updated

--- network_security/components/data_validation.py
+++ network_security/components/data_validation.py
@@ -1,4 +1,4 @@
-from network_security.entity.artifact_entity import Data_validation_artifact
+from network_security.entity.artifact_entity import Data_validation_artifact, Data_ingestion_artifact
 from network_security.entity.config_entity import Data_validation_config
 
 from network_security.exceptions.exception import NetworkSecurityException
@@ -9,9 +9,11 @@ import os
 import sys
 
 class DataValidation():
-    def __init__():
+    def __init__(self, data_ingestion_artifact : Data_ingestion_artifact, data_validation_config : Data_validation_config):
         try:
-            pass
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_validation_config = data_validation_config
+
         except Exception as e:
             raise NetworkSecurityException(e, sys)
     
